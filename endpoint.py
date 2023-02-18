662c15ff1049e2eb9ffb28cb020c4f986c7c976f
jeffx228 <jeffreyxu@college.harvard.edu>
2023-02-18 10:36:57 -0800

    API reqs for 6 coins

--- endpoint.py
+++ endpoint.py
@@ -1,20 +1,51 @@
 import requests
+import time
 
-# API endpoint
-url = 'https://api.pro.coinbase.com/products/BTC-USD/ticker'
+# Makes API call for given URL
+def hitAPI(url, coin):
+    # Send GET request to the endpoint
+    response = requests.get(url)
 
-# Send GET request to the endpoint
-response = requests.get(url)
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the response as JSON
+        data = response.json()
+        
+        # Extract the price from the data
+        price = data['price']
+        
+        # Print the price
+        # print(f"Current {coin} price: {price}")
 
-# Check if the request was successful
-if response.status_code == 200:
-    # Parse the response as JSON
-    data = response.json()
+        return price
     
-    # Extract the price from the data
-    price = data['price']
-    
-    # Print the price
-    print(f"Current BTC-USD price: {price}")
-else:
-    print(f"Request failed with status code {response.status_code}")
+    else:
+        print(f"Request failed with status code {response.status_code}")
+
+urls = ['https://api.pro.coinbase.com/products/BTC-USD/ticker',\
+        'https://api.pro.coinbase.com/products/ETH-USD/ticker',\
+        'https://api.pro.coinbase.com/products/BCH-USD/ticker',\
+        'https://api.pro.coinbase.com/products/LTC-USD/ticker',\
+        'https://api.pro.coinbase.com/products/LINK-USD/ticker',\
+        'https://api.pro.coinbase.com/products/XLM-USD/ticker'\
+        ]
+
+coins = ['Bitcoin', 'Ethereum', 'Bitcoin Cash', 'Litecoin', 'Chainlink', 'Stellar Lumens']
+# Bitcoin (BTC-USD): 'https://api.pro.coinbase.com/products/BTC-USD/ticker'
+# Ethereum (ETH-USD): https://api.pro.coinbase.com/products/ETH-USD/ticker
+# Bitcoin Cash (BCH-USD): https://api.pro.coinbase.com/products/BCH-USD/ticker
+# Litecoin (LTC-USD): https://api.pro.coinbase.com/products/LTC-USD/ticker
+# Chainlink (LINK-USD): https://api.pro.coinbase.com/products/LINK-USD/ticker
+# Stellar Lumens (XLM-USD): https://api.pro.coinbase.com/products/XLM-USD/ticker
+
+dict_list = {}
+
+for coin in coins:
+    dict_list[coin] = []
+
+
+while True: 
+    for url in urls:
+        coin = coins[urls.index(url)]
+        price = hitAPI(url, coin)
+
